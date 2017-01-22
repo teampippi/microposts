f3f8a117a2951f9c40b1c285f71995f3bd39d0ca
Haruki Sasaki <teampippi@gmail.com>
2017-01-22 04:18:19 +0000

    add profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+
   def show
-    @user = User.find(params[:id])
   end
   
   def new
@@ -17,9 +19,30 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Updated your Plofile"
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+  
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :location, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def correct_user
+    redirect_to root_path if @user != current_user
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,10 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :location, absence: true, 
+                       on: :create
+  validates :location, allow_blank: true, 
+                       length: { minimum: 2, maximum: 20 }, 
+                       on: :update
   has_secure_password
 end
